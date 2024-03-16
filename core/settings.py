231571628e37690458c3f5cb89996181affe505a
Shchekochihin Artem <gooartem93@gmail.com>
2024-03-16 19:00:51 +0300

    add first page

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'workshopapp.apps.WorkshopappConfig',
-    'newsapp.apps.NewsappConfig',
+    'workshopapp',
+    'newsapp',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
