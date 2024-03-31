d33daa525466142c1044a2af9893ccef52347d3b
Artem Shchekochikhin <gooartem93@gmail.com>
2024-03-31 21:07:00 +0300

    settings.py to local and project

--- workshop/workshop/settings.py
+++ workshop/workshop/settings.py
@@ -19,13 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)(jp$+kt#%-j4bf*%m1nkr$47izs_g(c%kku+kxt-_0e_wv94'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -74,12 +68,7 @@ WSGI_APPLICATION = 'workshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
