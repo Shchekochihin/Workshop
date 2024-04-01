11ecad84ff72f100391f366010ee327782ec22f8
Shchekochihin Artem <gooartem93@gmail.com>
2024-04-02 00:20:33 +0300

    edit prod_settings.py and local_settings.py

--- workshop/workshop/prod_settings.py
+++ workshop/workshop/prod_settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b)(jp$+kt#%-j4bf*%m1nkr$47izs_g(c%kku+kxt-_0e_wv94'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 DATABASES = {
     'default': {
