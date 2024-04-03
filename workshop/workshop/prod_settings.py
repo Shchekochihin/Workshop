6576c24a59c8f81af94150e70ddeed2096097206
Artem Shchekochikhin <gooartem93@gmail.com>
2024-04-03 11:21:01 +0300

    settings.py to local and project

--- workshop/workshop/prod_settings.py
+++ workshop/workshop/prod_settings.py
@@ -12,7 +12,11 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '<dbname>',
+        'USER': 'postgres',
+        'PASSWORD': '<password>',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
