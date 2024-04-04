6bfb638fd59290c4b6058e392f04c24f8429390b
Artem Shchekochikhin <gooartem93@gmail.com>
2024-04-04 09:26:45 +0300

    correct prod_settings.py

--- workshop/workshop/prod_settings.py
+++ workshop/workshop/prod_settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)(jp$+kt#%-j4bf*%m1nkr$47izs_g(c%kku+kxt-_0e_wv94'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql.psycopg2',
-        'NAME': '<workshop>',
+        'NAME': '',
         'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost',
