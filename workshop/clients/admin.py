bca8eaf163f34d0b02c8c40e407b133500d78d34
Artem Shchekochikhin <gooartem93@gmail.com>
2024-04-17 01:26:22 +0300

    views clients

--- workshop/clients/admin.py
+++ workshop/clients/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import PhoneNumbers, Clients
 
-
 admin.site.register(PhoneNumbers)
 admin.site.register(Clients)
--- workshop/clients/views.py
+++ workshop/clients/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
 
 def index(request):
-    return render(request, 'clients/index.html')
+    return HttpResponse("Hello")
