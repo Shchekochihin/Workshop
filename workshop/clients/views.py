2e94b75276752c5841df624ef306ff8076459e03
Shchekochihin Artem <gooartem93@gmail.com>
2024-04-19 07:00:31 +0300

    correct

--- workshop/clients/views.py
+++ workshop/clients/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from .models import *
 
 
 def index(request):
-    client = Clients.objects.get(id=1)
-    return HttpResponse("Hello {}".format(client.first_name))
+    return HttpResponse("Hello {}")
+
+
+def client(request, client_id):
+    return HttpResponse(f"Hello client {client_id}")
--- workshop/workshop/settings.py
+++ workshop/workshop/settings.py
@@ -24,8 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clients',
-    'gallery',
+    'clients.apps.ClientsConfig',
+    'gallery.apps.GalleryConfig',
 ]
 
 MIDDLEWARE = [
