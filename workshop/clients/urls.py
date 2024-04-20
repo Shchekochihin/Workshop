a812d63ce6d83f4bee49fa6157526af8affa6ce0
Artem Shchekochikhin <gooartem93@gmail.com>
2024-04-21 00:52:43 +0300

    edit

--- workshop/clients/urls.py
+++ workshop/clients/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='clients'),
     path('client/<int:client_id>/', views.client),
 ]
