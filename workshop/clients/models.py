1a6ddeea4a7f9dad4e30fc930563bf5d58a5a604
Artem Shchekochikhin <gooartem93@gmail.com>
2024-04-28 11:43:25 +0300

    correct models clients field

--- workshop/clients/models.py
+++ workshop/clients/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 # Create your models here.
 class PhoneNumbers(models.Model):
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, unique=True)
 
 
 class Clients(models.Model):
     first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    avatar = models.ImageField(upload_to='clients_avatar')
+    last_name = models.CharField(max_length=30, blank=True)
+    avatar = models.ImageField(upload_to='clients_avatar', blank=True)
     telephone = models.ForeignKey(PhoneNumbers, on_delete=models.SET_NULL, null=True)
-    client_location = models.CharField(max_length=100)
-    description = models.TextField
+    client_location = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
